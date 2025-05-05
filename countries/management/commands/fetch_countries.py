15117c22e05f6c69db12df33159baf0ccc00c37f
rahim0004 <88199855+rahim0004@users.noreply.github.com>
2025-05-05 07:24:05 +0600

    Implement data fetching script for REST Countries API

---
+++ countries/management/commands/fetch_countries.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+import requests
+from countries.models import Country
+
+class Command(BaseCommand):
+    help = 'Fetch and store country data from REST Countries API'
+
+    def handle(self, *args, **kwargs):
+        response = requests.get('https://restcountries.com/v3.1/all')
+        if response.status_code == 200:
+            countries = response.json()
+            for country in countries:
+                Country.objects.update_or_create(
+                    cca2=country.get('cca2'),
+                    defaults={
+                        'name': country.get('name', {}).get('common', ''),
+                        'capital': ', '.join(country.get('capital', [''])),
+                        'population': country.get('population', 0),
+                        'timezone': ', '.join(country.get('timezones', [''])),
+                        'flag': country.get('flags', {}).get('png', ''),
+                        'region': country.get('region', ''),
+                        'languages': country.get('languages', {})
+                    }
+                )
+            self.stdout.write(self.style.SUCCESS('Successfully fetched and stored country data'))
+        else:
+            self.stdout.write(self.style.ERROR('Failed to fetch country data'))
